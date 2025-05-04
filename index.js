eca4ef09d80eb52ca47ed6a199064beb1a22e083
Mohammed Hussain <mohammedhussain7339@gmail.com>
2025-05-04 12:45:50 +0530

    domain set

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ import cors from "cors";
 import connectToDb from "./connection/dbConnection.js"; // Ensure correct file path and `.js` extension
 import articleRoutes from "./routes/articleRoutes.js"
 const app = express();
-app.use(cors());
+const BASE_URL = process.env.BASE_URL ||'http://localhost:3000';
+
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', // Dev environment
+    'https://blognest-webs.vercel.app',
+    BASE_URL 
+  ],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" })); // Allow Next.js frontend
+// app.use(cors({ origin: "http://localhost:3000" })); // Allow Next.js frontend
 
 // Async function to start server
 const startServer = async () => {
